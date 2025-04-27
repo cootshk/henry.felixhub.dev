6c13a2b64b666fdc2f440f5a5fc3cc51e2ce7349
Henry <cootshk@duck.com>
2025-04-27 01:26:31 -0500

    lx in nix flake

--- flake.nix
+++ flake.nix
@@ -3,13 +3,31 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    lux = {
+      url = "github:nvim-neorocks/lux";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs }: {
+  outputs = { self, nixpkgs, lux, ... } @ inputs:
+  let
+    pkgs = nixpkgs.legacyPackages.x86_64-linux;
+  in
+  {
 
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+    packages = { 
+      inherit (lux.packages);
+      x86_64-linux.default = lux.packages.x86_64-linux.default;
+    };
+    # devShells = lux.devShells;
+    devShells.x86_64-linux.lux = lux.devShells.x86_64-linux.default;
+    devShells.x86_64-linux.default = pkgs.mkShell {
+      name = "felixhub devshell";
+      buildInputs = [
+        lux.packages.x86_64-linux.default
+        pkgs.luajit
+      ];
+    };
 
   };
 }
